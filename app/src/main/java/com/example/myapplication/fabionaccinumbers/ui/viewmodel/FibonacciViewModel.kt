64e84702dc8e7001b5f91a8db7ab50f538ba11c0
Uli Luckas <uluckas@users.noreply.github.com>
2022-01-28 08:22:53 +0100

    Improvements. (#1)
    I also thought it would be cleaner to not give the view model to the composable. But only the state.
    Reasoning: State is for compose what view model is for activity/fragment

--- app/src/main/java/com/example/myapplication/fabionaccinumbers/ui/viewmodel/FibonacciViewModel.kt
+++ app/src/main/java/com/example/myapplication/fabionaccinumbers/ui/viewmodel/FibonacciViewModel.kt
@@ -1,65 +1,52 @@
 package com.example.myapplication.fabionaccinumbers.ui.viewmodel
 
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.math.BigInteger
 
 class FibonacciViewModel : ViewModel() {
-    val mFibonacciNumbersState = mutableStateListOf(BigInteger("0"), BigInteger("1"))
-    private var nextFibonacciIndex = 2
+    val mFibonacciNumbersState: SnapshotStateList<BigInteger> = mutableStateListOf(BigInteger("0"), BigInteger("1"))
 
     init {
-        getNextFibonacciNumber()
+        produceFibonacciNumbers(2)
     }
 
-    private fun getNextFibonacciNumber() {
-        viewModelScope.launch(Dispatchers.IO) {
-            delay(100)
+    fun produceFibonacciNumbers(fibonacciIndex: Int) {
+        viewModelScope.launch(Dispatchers.Main) {
+            // Null means no more fibonacci numbers
             getNextFibonacciNumberRepository(
-                mFibonacciNumbersState[nextFibonacciIndex - 1],
-                mFibonacciNumbersState[nextFibonacciIndex - 2]
-            ).collect {
-                it.let { fibonacciNumber ->
-                    if (fibonacciNumber > BigInteger.ZERO) {
-                        withContext(Dispatchers.Main) {
-                            mFibonacciNumbersState.add(fibonacciNumber)
-                            nextFibonacciIndex++
-                            getNextFibonacciNumber()
-                        }
-                    }
-                }
+                mFibonacciNumbersState[fibonacciIndex - 1],
+                mFibonacciNumbersState[fibonacciIndex - 2]
+            )?.let { fibonacciNumber ->
+                mFibonacciNumbersState.add(fibonacciNumber)
+                produceFibonacciNumbers(fibonacciIndex + 1)
             }
         }
     }
 
+    // Android Studio does not understand that Thread.sleep() is executed in background
+    @Suppress("BlockingMethodInNonBlockingContext")
+    /**
+     * Calculate next fibonacci number after [previousLastFibonacciNumber] and
+     * [lastFibonacciNumber]
+     *
+     * @return next Fibonacci number or null if limit of ULong.MAX_VALUE is exceeded
+     */
     private suspend fun getNextFibonacciNumberRepository(
         lastFibonacciNumber: BigInteger,
         previousLastFibonacciNumber: BigInteger
-    ): Flow<BigInteger> =
-        flow {
-            val output = calculateNextFibonacciNumber(
-                lastFibonacciNumber,
-                previousLastFibonacciNumber
-            )
-            if (output <= BigInteger(ULong.MAX_VALUE.toString())) {
-                emit(
-                    output
-                )
-            } else {
-                emit(BigInteger.ZERO)
-            }
+    ): BigInteger? = withContext(Dispatchers.IO) {
+        Thread.sleep(500)
+        val output = lastFibonacciNumber + previousLastFibonacciNumber
+        if (output <= BigInteger(ULong.MAX_VALUE.toString())) {
+            return@withContext output
+        } else {
+            return@withContext null
         }
-
-    private fun calculateNextFibonacciNumber(
-        lastFibonacciNumber: BigInteger,
-        previousLastFibonacciNumber: BigInteger
-    ): BigInteger = lastFibonacciNumber + previousLastFibonacciNumber
+    }
 }
